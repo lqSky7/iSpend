ac12d6f293071ab20ddc8255e2c8605d6eb7fca6
ca5 <catinice@outlook.com>
2025-12-08 21:12:53 +0530

    .sheets
    Signed-off-by: ca5 <catinice@outlook.com>

--- iSpend/ContentView.swift
+++ iSpend/ContentView.swift
@@ -7,17 +7,32 @@
 
 import SwiftUI
 
+
+struct Sheet : View {
+    var body : some View {
+        Text("Secpnd sheet")
+        Text("hiiiiiï")
+    }
+}
+
 @Observable
 class User {
     var name: String = "hii"
 }
 
 struct ContentView: View {
+    @State private var showingSecondSheet = false
     @State var user1: User = User()
     var body: some View {
         VStack {
             Text("\(user1.name) bye")
             TextField("the name is: ",text: $user1.name)
+            Button("SHOW SECOND SHEET"){
+                showingSecondSheet.toggle()
+            }
+            .sheet(isPresented: $showingSecondSheet){
+                Sheet()
+            }
         }
         .padding()
     }
