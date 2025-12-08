ab9b03d2d9df5efa502faf237995a86725ed5257
ca5 <catinice@outlook.com>
2025-12-08 20:23:44 +0530

    Initial Commit

---
+++ iSpend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSpend
+//
+//  Created by ca5 on 08/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSpend/iSpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpendApp.swift
+//  iSpend
+//
+//  Created by ca5 on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iSpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
